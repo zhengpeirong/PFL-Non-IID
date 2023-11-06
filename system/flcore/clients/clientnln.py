0fc749b4dfd45284826eb7c07c89cd3e5b9fa8c2
zhengpeirong <1592635674@qq.com>
2023-11-06 07:47:11 +0000

    添加初始化代码

--- system/flcore/clients/clientnln.py
+++ system/flcore/clients/clientnln.py
@@ -15,8 +15,88 @@ class clientNLN(Client):
 
         self.loss = nn.CrossEntropyLoss()
         self.optimizer = MySGD(
-            self.model.parameters(), lr=self.learning_rate)
+            self.init_params_lr_by_layer(), lr=self.learning_rate)
+        # NOTE：原base的初始：
         
+        self.conf = conf  # 配置信息
+        self.local_model = copy.deepcopy(model)  # 本地模型
+        self.client_id = id  # 客户端id
+        self.train_dataset = train_dataset  # 训练集
+        self.train_dataset_idcs = train_dataset_idcs # 训练集下标
+        self.dataset_size = self.train_dataset_idcs.size # 不需要自行数数self.get_dataset_size()
+        self.train_loader = torch.utils.data.DataLoader(self.train_dataset, batch_size=conf['batch_size'],
+                                                        num_workers=self.conf["num_workers"],
+                                                        # pin_memory=True,
+                                                        # drop_last=True,
+                                                        sampler=torch.utils.data.sampler.SubsetRandomSampler(
+                                                            self.train_dataset_idcs))
+        # SubsetRandomSampler 随机采样from a given list of indices train_dataset_idcs.
+        if torch.cuda.is_available():
+            self.local_model = self.local_model.cuda()
+        self.conv_layers = [module for name, module in self.local_model.named_modules() if (isinstance(module, torch.nn.Conv2d) and module.kernel_size != (1, 1)) or isinstance(module, torch.nn.Linear) ]
+        self.conv_layers_names = [name for name, module in self.local_model.named_modules() if (isinstance(module, torch.nn.Conv2d) and module.kernel_size != (1, 1)) or isinstance(module, torch.nn.Linear) ]
+        self.num_conv = len(self.conv_layers) #权重层数量
+        self.num_class = len(self.train_loader.dataset.class_to_idx) #类别数量
+        self.param_dict = {}
+        self.param_dict['weight_decay'] = self.conf["weight_decay"]
+        self.param_dict['momentum'] = self.conf["momentum"]
+        self.param_dict['lr'] = self.conf['lr']
+        self.device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
+
+        # NOTE: fednln特定的初始化
+        # 训练数据加载器
+        self.activation = []# 保存每一个权重层的输出
+        # 获取所有权重层，resnet18的17个卷积层和1个全连接层
+        self.conv_layers = [module for name, module in self.local_model.named_modules() if (isinstance(module, torch.nn.Conv2d) and module.kernel_size != (1, 1)) or isinstance(module, torch.nn.Linear) ]
+        self.conv_layers_names = [name for name, module in self.local_model.named_modules() if (isinstance(module, torch.nn.Conv2d) and module.kernel_size != (1, 1)) or isinstance(module, torch.nn.Linear) ]
+        self.num_conv = len(self.conv_layers) #权重层数量
+        self.num_class = len(self.train_loader.dataset.class_to_idx) #类别数量
+        # result_list (类别10*权重层数量18)的列表，存储列表activation
+        self.result_list = [[[] for _ in range(self.num_conv)] for _ in range(self.num_class)]
+        self.TF = [[[] for _ in range(self.num_conv)] for _ in range(self.num_class)]
+        self.channel_lr_multiplier = [[] for _ in range(self.num_conv)]
+        self.verbose =  False # 仅仅用于快速测试
+        self.label_proportions = {}
+        self.inc = self.conf["mul"] # 让缩放因子的最小值变成1
+        self.pre_mul = self.conf["pre_mul"]  # 等于1时，数学上不能计算，不预处理。
+        self.last_layer_index = -1  # 用于打印最后一层的缩放因子
+        self.dataset_size = self.train_dataset_idcs.size # 不需要自行数数self.get_dataset_size()
+        self.param_dict.pop('lr')
+    
+    def init_params_lr_by_layer(self):
+        r"""conv_layers_names->params_lr
+        每次训练之前初始化，确定哪些参数需要训练，初始学习率缩放全为1
+        主要是为了将params分组，这样每一层都可以设置不同的学习率，适合CL方法与RS方法"""
+        params_lr = []
+        param_dict = copy.deepcopy(self.param_dict)
+        # HACK: 初始化时，自带lr
+        if param_dict.get('lr') == None:
+            param_dict['lr'] = self.conf['lr']
+        for i, layer_name in enumerate(self.conv_layers_names): 
+            found_layer_weight = False
+            found_layer_bias = False
+            for name, param in self.local_model.named_parameters():
+                if found_layer_weight and found_layer_bias: continue # 'conv'层有weight和bias
+                if layer_name == name[:len(layer_name)]:
+                    if 'weight' in name:
+                        params_lr.append({'params': param, **param_dict})
+                        found_layer_weight = True 
+                    elif 'bias' in name:
+                        params_lr.append({'params': param, **param_dict})
+                        found_layer_bias = True
+        self.last_layer_index = len(params_lr)-1
+        # 是否只设置权重层的学习率
+        if not self.conf['only_train_convs']:
+            for name, param in self.local_model.named_parameters():# 遍历本地模型的所有参数
+                param_handled = False # 标志是否已经被上面的处理
+                for handled_param_group in params_lr:
+                    if param is handled_param_group["params"]:
+                        param_handled = True
+                        break
+                if not param_handled:
+                    params_lr.append({"params": param, **param_dict})
+        return params_lr
+    
     def train(self):
         trainloader = self.load_train_data()
         start_time = time.time()
@@ -45,8 +125,8 @@ class clientNLN(Client):
 
         # self.model.cpu()
 
-        if self.learning_rate_decay:
-            self.learning_rate_scheduler.step()
+        # if self.learning_rate_decay:
+        #     self.learning_rate_scheduler.step()
 
         self.train_time_cost['num_rounds'] += 1
         self.train_time_cost['total_cost'] += time.time() - start_time
--- system/flcore/servers/servernln.py
+++ system/flcore/servers/servernln.py
@@ -16,8 +16,68 @@ class FedNLN(Server):
         print("Finished creating server and clients.")
 
         # self.load_model()
+        # NOTE: base的初始化：
+        self.conf = conf
+        self.num_class = len(eval_dataset.class_to_idx)
+        self.global_model = self.init_model(conf['model_name'],self.num_class)
+        self.eval_loader = torch.utils.data.DataLoader(eval_dataset, 
+                                                       batch_size=self.conf["batch_size"],
+                                                       num_workers=self.conf["num_workers"],
+                                                       shuffle=True,
+                                                    #    pin_memory=True,
+                                                       )  # 创建测试集加载器用于测试最终的聚合模型
+        self.device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
+        self.global_model = self.global_model.to(self.device)
 
+        # NOTE: fednln特定的初始化
+        self.conv_layers = [module for name, module in self.global_model.named_modules() if (isinstance(module, torch.nn.Conv2d) and module.kernel_size != (1, 1)) or isinstance(module, torch.nn.Linear) ]
+        self.conv_layers_names = [name for name, module in self.global_model.named_modules() if (isinstance(module, torch.nn.Conv2d) and module.kernel_size != (1, 1)) or isinstance(module, torch.nn.Linear) ]
+        self.num_conv = len(self.conv_layers)
+        self.num_class = len(self.eval_loader.dataset.class_to_idx)
+        # self.result_list = [[torch.Tensor() for _ in range(column)] for _ in range(row)]
+        self.result_list = [[np.array([], dtype=np.float64) for _ in range(self.num_conv)] for _ in range(self.num_class)]
+        # 空壳子：channel_frequency仅仅用来创建新的相同维度的变量。
+        self.channel_frequency = [[ np.array([], dtype=np.float32) for _ in range(self.num_conv)] for _ in range(self.num_class)]
+        self.TF = copy.deepcopy(self.channel_frequency)
+        self.TF_IDF = copy.deepcopy(self.channel_frequency)
+        self.verbose = self.conf["debug_mode"]
+        self.IDF = [np.array([]) for _ in range(self.num_conv)] 
+        self.layer_mul = np.ones(self.num_conv,dtype=np.float32)+0.01
+        conv_layers_dimension_list= [module.weight.shape[0] for name, module in self.global_model.named_modules() if (isinstance(module, torch.nn.Conv2d) and module.kernel_size != (1, 1)) or isinstance(module, torch.nn.Linear) ]
+        self.formula_layer_mul =  1 + np.log10(np.array(conv_layers_dimension_list) )* 0.75
+        #mul的计算，可以用fitting.py拟合
+        for index, out_features in enumerate(conv_layers_dimension_list):
+            self.formula_layer_mul[index] =  1 + 0.45 * (index+1)/self.num_conv + 0.3*np.log10(np.array(out_features))
+        self.device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
 
+    def init_model(self,model_name,num_class):
+        if hasattr(myCNN, model_name): # 先查看是否本地已经构建。
+            model = eval('myCNN.{}()'.format(model_name)) 
+        else:
+            model = eval('models.{}()'.format(model_name))  # 从配置文件获得模型名称并创建服务器模型
+            if "resnet" in model_name:
+                # 获取原始模型的全连接层的输入维度
+                in_features = model.fc.in_features
+                # 定义新的全连接层
+                new_fc = nn.Linear(in_features,num_class)  # 10是CIFAR-10数据集的类别数量
+                # 替换ResNet18的最后一层
+                model.fc = new_fc
+        self.init_weights(model)  # 初始化权重
+        return model
+    def init_weights(self,model):
+        for m in model.modules():
+            if isinstance(m, (nn.Conv2d, nn.Linear)):
+                torch.nn.init.kaiming_normal_(m.weight, nonlinearity='relu')
+                print(f'{m.__class__.__name__}.weight({m.weight.shape}) 已初始化')
+                if m.bias is not None:
+                    torch.nn.init.zeros_(m.bias)
+                    print(f'{m.__class__.__name__}.bias({m.bias.shape}) 已初始化')
+            elif isinstance(m, nn.BatchNorm2d) or isinstance(m,nn.GroupNorm):
+                torch.nn.init.constant_(m.weight, 1.0)
+                torch.nn.init.zeros_(m.bias)
+                print(f'{m.__class__.__name__}.weight({m.weight.shape}) 已初始化')
+                print(f'{m.__class__.__name__}.bias({m.bias.shape}) 已初始化')
+    
     def train(self):
         for i in range(self.global_rounds+1):
             self.selected_clients = self.select_clients()
